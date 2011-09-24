8464fd06ff6374f0f22aabe09b9f90196d3ab1bf
Alexandr Yakubenko <SmitBLR@Gmail.com>
2011-09-24 22:57:37 +0300

    Global fixing and advert support. jquery-rails added

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function comment_notify(message) {
+    var notice_div = $("#commentnotice");
+    notice_div.html(message).hide().slideToggle("slow", function () {
+        setTimeout(function () {
+            notice_div.slideToggle("slow")
+        },2000)
+    });
+}
