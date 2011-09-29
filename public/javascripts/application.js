a11041c37dace6c9eb6877fa9fce9d8046197c52
Alexandr Yakubenko <SmitBLR@Gmail.com>
2011-09-29 13:44:27 +0300

    IExplore footerfix. Tiser added.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,8 @@
+function teasetnet_init() {
+    a = $("#teasernet_291256");
+    $("#teasernet").css('margin-left', -((($(document).width()-640)/2-a.width())/2+a.width()));
+};
+
 function comment_notify(message) {
     var notice_div = $("#commentnotice");
     notice_div.html(message).hide().slideToggle("slow", function () {
