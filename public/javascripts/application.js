e58910b831693d0d8e3789a62b63168d07b28671
Alexandr Yakubenko <smitblr@gmail.com>
2011-10-26 01:30:46 +0300

    added google_skyscraper animatet advert block

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,41 @@
-function teasetnet_init() {
-    a = $("#teasernet_291256");
-    $("#teasernet").css('margin-left', -((($(document).width()-640)/2-a.width())/2+a.width()));
+function move_advert(element, y_offset, margin_top)
+{
+    if (Math.abs(margin_top.val - y_offset.val) > 1)
+    {
+        if (margin_top.val < y_offset.val) { k = 1 } else { k = -1 }
+        margin_top.val += (Math.abs(Math.round((y_offset.val - margin_top.val) / 6))) * k;
+        element.css('margin-top', margin_top.toString() + "px");
+    }
+    else
+    {
+        clearInterval(ki);
+    }
+}
+
+function advert_animate()
+{
+    clearInterval(ki);
+    a = $("#teasernet");
+    mt = a.css('margin-top');
+    margin_top = {
+        val: parseInt(mt.substr(0,mt.length - 2)),
+        toString: function () { return this.val.toString() }
+    };
+    y_start_offset = Math.round(($(window).height() - a.height()) / 2);
+    y_offset = {
+        val: window.pageYOffset + y_start_offset,
+        toString: function () { return this.val.toString() }
+    };
+    ki = setInterval('move_advert(a, y_offset, margin_top);', 20);
+}
+
+function skyscraper_init() {
+    a = $("#teasernet");
+    a.css('margin-left', Math.round(((($(window).width() - $("#wrapper").width()) / 2 - a.width()) / 2)));
+    a.css('margin-top', -a.height());
+    ki = 0;
+    advert_animate();
+    window.onscroll = advert_animate;
 };
 
 function comment_notify(message) {
