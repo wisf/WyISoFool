3ca438002e04533d74a7dc487aa74a6b6a8412e4
Alexandr Yakubenko <smitblr@gmail.com>
2011-11-26 01:20:45 +0300

    auto-scroll buttons

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -51,3 +51,28 @@
                     },200);
                 });
             });
+
+   $(function() {
+       var $elem = $('#page');
+
+       $('#nav_up').fadeIn('slow');
+       $('#nav_down').fadeIn('slow');
+
+       $(window).bind('scrollstart', function(){
+           $('#nav_up,#nav_down').stop().animate({'opacity':'0.2'});
+       });
+       $(window).bind('scrollstop', function(){
+           $('#nav_up,#nav_down').stop().animate({'opacity':'1'});
+       });
+
+       $('#nav_down').click(
+           function (e) {
+               $('html, body').animate({scrollTop: $elem.height()}, 800);
+           }
+       );
+       $('#nav_up').click(
+           function (e) {
+               $('html, body').animate({scrollTop: '0px'}, 800);
+           }
+       );
+   });
