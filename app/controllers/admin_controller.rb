1c122129e9340ee8714239ef8a4b04cffc37b868
Alexandr Yakubenko <smitblr@gmail.com>
2011-12-17 14:12:51 +0300

    AdminController refactoring

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,20 +1,16 @@
 # encoding: UTF-8
 class AdminController < ApplicationController
   def index
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @stories }
-    end
   end
 
   def signin    
     session[:user] = User.authenticate(params[:user][:username], params[:user][:password]).id
-    redirect_to :controller => 'stories' , :notice => 'Добро пожаловать в мир дур, о всемогущий ' + params[:user][:username] + '!'
+    redirect_to :back , :notice => "Добро пожаловать в мир дур, о всемогущий #{params[:user][:username]}!"
   end
 
   def signout
     session[:user] = nil
-    redirect_to :controller => 'stories'
+    redirect_to :back
   end
 
 end
